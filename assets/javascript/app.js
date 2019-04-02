8031ed5652d8df1c19859c991769bb988ee3f6b9
ECarlson <ericcarlson4@outlook.com>
2019-04-02 00:43:48 -0700

    adding some more questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,12 +6,18 @@ var startTimer = true;
 var IdTimer = "";
 
 var questions = {
-    q1: "Which champion is not Demacian?",
+    q1: "Which champion is NOT Demacian?",
     q2: "Which champion is consumed with vengeance against Thresh for holding his wifes soul?",
     q3: "What item clears almost all forms of crowd control?",
-    q4: "Which summoner spell is the most common in League of Legends games?",
+    q4: "Which summoner spell is the most commonly used in League of Legends games?",
     q5: "When does rift hearld despawn on the map?",
     q6: "What elemental drake regenerates health over time?",
+    q7: "How many towers does each team have in each summoners rift game?",
+    q8: "What champion does not have an official solo in the League of Legends music video 'Popstars' by KD/A",
+    q9: "Which champion is also known as 'The Eternal Nightmare'?",
+    q10: "Which NBA team does NOT own a professional League of Legends team?",
+    q11: "Which single item is the most expensive in the game?",
+    q12: "How many dragons can you have before Elder Dragon spawns?",
 
 
 
@@ -28,6 +34,13 @@ var options = {
     q4: ["Ghost" , "Teleport" , "Flash" , "Ignite"],
     q5: ["14:55" , "19:55" , "24:55" , "29:55"],
     q6: ["Mountain" , "Infernal" , "Wind" , "Ocean"],
+    q7: ["8" , "11" , "13" , "15"],
+    q8: ["Kai'sa" , "Ahri" , "Akali" , "Evelyyn"],
+    q9: ["Zed" , "Karthus" , "Diana" , "Nocturne"],
+    q10: ["Houston Rockets" , "Golden State Warriors" , "Los Angeles Lakers" , "Cleveland Caveliers"],
+    q11: ["Serrated Dirk" , "Needlessly Large Rod" , "Boots of Speed" , "Ruby Crystal"],
+    q12: ["3" , "4" , "5" , "6"],
+
 
 
 
@@ -47,6 +60,14 @@ var answers = {
     q4: "Flash",
     q5: "19:55",
     q6: "Ocean",
+    q7: "11",
+    q8: "Kai'sa",
+    q9: "Nocturne",
+    q10: "Los Angeles Lakers",
+    q11: "Needlessly Large Rod",
+    q12: "6",
+
+
 
 
 
