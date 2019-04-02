ba7417727d44223abf4c564a00d256a4db1ede17
ECarlson <ericcarlson4@outlook.com>
2019-04-01 21:59:30 -0700

    added some questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,3 +5,48 @@ var timer = 30;
 var startTimer = true;
 var IdTimer = "";
 
+var questions = {
+    q1: "Which champion is not Demacian?",
+    q2: "Which champion is consumed with vengeance against Thresh for holding his wifes soul?",
+    q3: "What item clears all forms of crowd control?",
+
+
+
+
+
+
+
+
+}
+var options = {
+    q1: ["Garen", "Fiora" , "Katarina" , "Lux"],
+    q2: ["Lucian" , "Rakan" , "Shen" , "Swain"],
+    q3: ["Statikk Shiv" , "Quicksilver Sash" , "Adaptive Helm" , "Phantom Dancer"],
+
+
+
+
+
+
+
+
+
+
+}
+
+var answers = {
+    q1: "Katarina",
+    q2: "Lucian",
+    q3: "Quicksilver Sash",
+    
+
+
+
+
+
+
+
+
+
+}    
+
