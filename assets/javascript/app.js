e2fdd5a7d8daa363a70c184b083e9b123c7d1c54
ECarlson <ericcarlson4@outlook.com>
2019-04-01 22:14:46 -0700

    added more questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,10 @@ var IdTimer = "";
 var questions = {
     q1: "Which champion is not Demacian?",
     q2: "Which champion is consumed with vengeance against Thresh for holding his wifes soul?",
-    q3: "What item clears all forms of crowd control?",
+    q3: "What item clears almost all forms of crowd control?",
+    q4: "Which summoner spell is the most common in League of Legends games?",
+    q5: "When does rift hearld despawn on the map?",
+    q6: "What elemental drake regenerates health over time?",
 
 
 
@@ -22,6 +25,9 @@ var options = {
     q1: ["Garen", "Fiora" , "Katarina" , "Lux"],
     q2: ["Lucian" , "Rakan" , "Shen" , "Swain"],
     q3: ["Statikk Shiv" , "Quicksilver Sash" , "Adaptive Helm" , "Phantom Dancer"],
+    q4: ["Ghost" , "Teleport" , "Flash" , "Ignite"],
+    q5: ["14:55" , "19:55" , "24:55" , "29:55"],
+    q6: ["Mountain" , "Infernal" , "Wind" , "Ocean"],
 
 
 
@@ -38,7 +44,10 @@ var answers = {
     q1: "Katarina",
     q2: "Lucian",
     q3: "Quicksilver Sash",
-    
+    q4: "Flash",
+    q5: "19:55",
+    q6: "Ocean",
+
 
 
 
