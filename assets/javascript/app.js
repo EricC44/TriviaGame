8ffe7e4676fee3264e4818c869fe236bb44d237a
ECarlson <ericcarlson4@outlook.com>
2019-07-09 01:57:41 -0700

    linted the page so it was cleaner

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,177 +1,162 @@
 
-//This is to make sure the jQuery doesnt start early.
-$(document).ready(function(){
+// This is to make sure the jQuery doesnt start early.
+$(document).ready(function () {
+  document.getElementById('time-left').style.display = 'none'
+  // This is to listen for a mouse click with the Id "start"
+  var startGame = document.getElementById('start').addEventListener('click', startGame)
 
-document.getElementById("time-left").style.display = "none";
-//This is to listen for a mouse click with the Id "start"
-var startGame = document.getElementById("start").addEventListener("click" , startGame);
+  // document.addEventListener("click" , currentAnswer);
 
-//document.addEventListener("click" , currentAnswer);
+  // Global variables;
 
-//Global variables;
+  // var correctGuess = 0
+  // var incorrectGuess = 0
+  // var unanswered = 0
+  var currentQuestion = 0
+  var timer = 30
+  var startTimer = true
+  var IdTimer = ''
 
-var correctGuess = 0;
-var incorrectGuess = 0;
-var unanswered = 0;
-var currentQuestion = 0;
-var timer =  30;
-var startTimer = true;
-var IdTimer = "";
-
-var questions =[{
-    question: "color of sky",
-    answers: ['blue', 'red','green', 'pink'],
+  var questions = [{
+    question: 'color of sky',
+    answers: ['blue', 'red', 'green', 'pink'],
     correctAnswer: 'blue'
-  },{
-    question: "color of sky",
-    answers: ['blue', 'red','green', 'pink'],
+  }, {
+    question: 'color of sky',
+    answers: ['blue', 'red', 'green', 'pink'],
     correctAnswer: 'blue'
 
-  },{
-    question: "color of sky",
-    answers: ['blue', 'red','green', 'pink'],
+  }, {
+    question: 'color of sky',
+    answers: ['blue', 'red', 'green', 'pink'],
     correctAnswer: 'blue'
   }
-]
+  ]
 
+  // //This is the Questions array
+  // var questions = {
+  //     q1: "Which champion is NOT Demacian?",
+  //     q2: "Which champion is consumed with vengeance against Thresh for holding his wifes soul?",
+  //     q3: "What item clears almost all forms of crowd control?",
+  //     q4: "Which summoner spell is the most commonly used in League of Legends games?",
+  //     q5: "When does rift hearld despawn on the map?",
+  //     q6: "What elemental drake regenerates health over time?",
+  //     q7: "How many towers does each team have in each summoners rift game?",
+  //     q8: "What champion does NOT have an official solo in the League of Legends music video 'Popstars' by KD/A",
+  //     q9: "Which champion is also known as 'The Eternal Nightmare'?",
+  //     q10: "Which NBA team does NOT own a professional League of Legends team?",
+  //     q11: "Which single item is the most expensive in the game?",
+  //     q12: "How many dragons can you have before Elder Dragon spawns?",
+  //     q13: "As of April 2nd 2019, which champion is considered newest? (excluding reworks)",
+  //     q14: "Which champions ult is called Shurima Shuffle?",
+  //     q15: "Who is Riot Games beloved Yordle and mascot?",
 
+  // }
+  // //This is the choices array
+  // var choices = [{
+  //     q1: ["Garen", "Fiora" , "Katarina" , "Lux"],
+  //     q2: ["Lucian" , "Rakan" , "Shen" , "Swain"],
+  //     q3: ["Statikk Shiv" , "Quicksilver Sash" , "Adaptive Helm" , "Phantom Dancer"],
+  //     q4: ["Ghost" , "Teleport" , "Flash" , "Ignite"],
+  //     q5: ["14:55" , "19:55" , "24:55" , "29:55"],
+  //     q6: ["Mountain" , "Infernal" , "Wind" , "Ocean"],
+  //     q7: ["8" , "11" , "13" , "15"],
+  //     q8: ["Kai'sa" , "Ahri" , "Akali" , "Evelyyn"],
+  //     q9: ["Zed" , "Karthus" , "Diana" , "Nocturne"],
+  //     q10: ["Houston Rockets" , "Golden State Warriors" , "Los Angeles Lakers" , "Cleveland Caveliers"],
+  //     q11: ["Serrated Dirk" , "Needlessly Large Rod" , "Boots of Speed" , "Ruby Crystal"],
+  //     q12: ["3" , "4" , "5" , "6"],
+  //     q13: ["Sylas" , "Zoe" , "Neeko" , "Kai'sa"],
+  //     q14: ["Ziggs" , "Jayce" , "Vi" , "Azir"],
+  //     q15: ["Ashe" , "Teemo" , "Yasuo" , "Jinx"],
+  // }]
+  // //This is the answers array
+  // var answers = {
+  //     q1: "Katarina",
+  //     q2: "Lucian",
+  //     q3: "Quicksilver Sash",
+  //     q4: "Flash",
+  //     q5: "19:55",
+  //     q6: "Ocean",
+  //     q7: "11",
+  //     q8: "Kai'sa",
+  //     q9: "Nocturne",
+  //     q10: "Los Angeles Lakers",
+  //     q11: "Needlessly Large Rod",
+  //     q12: "6",
+  //     q13: "Sylas",
+  //     q14: "Azir",
+  //     q15: "Teemo"
+  // }
 
-// //This is the Questions array
-// var questions = {
-//     q1: "Which champion is NOT Demacian?",
-//     q2: "Which champion is consumed with vengeance against Thresh for holding his wifes soul?",
-//     q3: "What item clears almost all forms of crowd control?",
-//     q4: "Which summoner spell is the most commonly used in League of Legends games?",
-//     q5: "When does rift hearld despawn on the map?",
-//     q6: "What elemental drake regenerates health over time?",
-//     q7: "How many towers does each team have in each summoners rift game?",
-//     q8: "What champion does NOT have an official solo in the League of Legends music video 'Popstars' by KD/A",
-//     q9: "Which champion is also known as 'The Eternal Nightmare'?",
-//     q10: "Which NBA team does NOT own a professional League of Legends team?",
-//     q11: "Which single item is the most expensive in the game?",
-//     q12: "How many dragons can you have before Elder Dragon spawns?",
-//     q13: "As of April 2nd 2019, which champion is considered newest? (excluding reworks)",
-//     q14: "Which champions ult is called Shurima Shuffle?",
-//     q15: "Who is Riot Games beloved Yordle and mascot?",
-
-// }
-// //This is the choices array
-// var choices = [{
-//     q1: ["Garen", "Fiora" , "Katarina" , "Lux"],
-//     q2: ["Lucian" , "Rakan" , "Shen" , "Swain"],
-//     q3: ["Statikk Shiv" , "Quicksilver Sash" , "Adaptive Helm" , "Phantom Dancer"],
-//     q4: ["Ghost" , "Teleport" , "Flash" , "Ignite"],
-//     q5: ["14:55" , "19:55" , "24:55" , "29:55"],
-//     q6: ["Mountain" , "Infernal" , "Wind" , "Ocean"],
-//     q7: ["8" , "11" , "13" , "15"],
-//     q8: ["Kai'sa" , "Ahri" , "Akali" , "Evelyyn"],
-//     q9: ["Zed" , "Karthus" , "Diana" , "Nocturne"],
-//     q10: ["Houston Rockets" , "Golden State Warriors" , "Los Angeles Lakers" , "Cleveland Caveliers"],
-//     q11: ["Serrated Dirk" , "Needlessly Large Rod" , "Boots of Speed" , "Ruby Crystal"],
-//     q12: ["3" , "4" , "5" , "6"],
-//     q13: ["Sylas" , "Zoe" , "Neeko" , "Kai'sa"],
-//     q14: ["Ziggs" , "Jayce" , "Vi" , "Azir"],
-//     q15: ["Ashe" , "Teemo" , "Yasuo" , "Jinx"],
-// }]
-// //This is the answers array
-// var answers = {
-//     q1: "Katarina",
-//     q2: "Lucian",
-//     q3: "Quicksilver Sash",
-//     q4: "Flash",
-//     q5: "19:55",
-//     q6: "Ocean",
-//     q7: "11",
-//     q8: "Kai'sa",
-//     q9: "Nocturne",
-//     q10: "Los Angeles Lakers",
-//     q11: "Needlessly Large Rod",
-//     q12: "6",
-//     q13: "Sylas",
-//     q14: "Azir",
-//     q15: "Teemo"
-// } 
-
-
-
-  function startGame() {
-    hideStart();
-    correctGuess = 0;
-    incorrectGuess = 0;
-    unanswered = 0;
-    currentQuestion = 0;
-    clearInterval(IdTimer);
-    startTimer = true;
+  function startGame () {
+    hideStart()
+    // correctGuess = 0
+    // incorrectGuess = 0
+    // unanswered = 0
+    currentQuestion = 0
+    clearInterval(IdTimer)
+    startTimer = true
     // if(startTimer = true) {
     // timerRunning();
     // }
-    //timerRunning();
+    // timerRunning();
 
     // displayTrivia();
-    document.getElementById("timer").innerText = timer;
-    
-    document.getElementById("time-left").style.display = "block";
-    firstQuestion();
-    //clearResults();
-    //timerRunning();
+    document.getElementById('timer').innerText = timer
+
+    document.getElementById('time-left').style.display = 'block'
+    firstQuestion()
+    // clearResults();
+    // timerRunning();
   }
 
-  function hideStart() {
-    document.getElementById("start").style.display = "none";
+  function hideStart () {
+    document.getElementById('start').style.display = 'none'
   }
   // function displayTrivia() {
   //   document.getElementById("trivia").style.display = "block";
   // }
-//This is to clear my results once you restart the game (working on)
-  //function clearResults() {
-  //document.getElementById("final-results").innerHTML = '';
-  //}
-  
+  // This is to clear my results once you restart the game (working on)
+  // function clearResults() {
+  // document.getElementById("final-results").innerHTML = '';
+  // }
 
+  // This function is suppose to start the first question and timer
+  function firstQuestion () {
+    timer = document.getElementById('time-left').innerText
+    document.getElementById('time-left').style.display = 'block'
 
-  //This function is suppose to start the first question and timer
-  function firstQuestion() {
-    timer = document.getElementById("time-left").innerText;
-    document.getElementById("time-left").style.display = "block";
-    
-    //This is just to make sure that the timer doesnt speed up
-    if(!startTimer) {
-        timer = setInterval(count , 1000);
-        
+    // This is just to make sure that the timer doesnt speed up
+    if (!startTimer) {
+      timer = setInterval(count, 1000)
+
     }
     // //This is hopefully putting the questions into the HTML as well as keeping to the question.
     // var questionLeagueOptions = Object.values(questions)[currentQuestion];
     // questionLeagueOptions = document.getElementById("choices").innerHTML;
     // console.log(questionLeagueOptions);
-    
 
     // var questionLeagueChoices = Object.values(choices)[currentQuestion];
     // console.log(questionLeagueChoices);
     // checkGuess();
-    
 
-    
-    
-    //This is supposed to append a button per possible choice
-    
-    
-  //   $.each(questionLeagueChoices, function(index , key){
-  //     $('#choices').append($('<button class="option btn btn-default btn-lg">'+key+'</button>'));
-  //     console.log(questionLeagueChoices);
-  //     console.log(index);
-      
-  // })
-      currentQuestion = 0;
-      document.getElementById("questionId").innerHTML="<h2>" + questions[0].question + "</h2>";
-      for(var i=0; i< questions[currentQuestion].answers.length; i++){
-        document.getElementById("choices").innerHTML = "<button class='answer-button' id='button' data-name=''" + questions[currentQuestion].answers[i]
-        + ">" + questions[currentQuestion].answers[i] + "</button>";
-        console.log(questions[i].answers);
-      }
-      //console.log();
-      
-    
+    // This is supposed to append a button per possible choice
 
+    //   $.each(questionLeagueChoices, function(index , key){
+    //     $('#choices').append($('<button class="option btn btn-default btn-lg">'+key+'</button>'));
+    //     console.log(questionLeagueChoices);
+    //     console.log(index);
+
+    // })
+    currentQuestion = 0
+    document.getElementById('questionId').innerHTML = '<h2>' + questions[0].question + '</h2>'
+    for (var i = 0; i < questions[currentQuestion].answers.length; i++) {
+      document.getElementById('choices').innerHTML = "<button class='answer-button' id='button' data-name=''" + questions[currentQuestion].answers[i] +
+        '>' + questions[currentQuestion].answers[i] + '</button>'
+      console.log(questions[i].answers)
+    }
+    // console.log();
   }
   // //This is hopefully the timer and final seconds and decrement
   // function timerRunning() {
@@ -179,16 +164,16 @@ var questions =[{
   //   if(timer > -1 && currentQuestion < Object.keys(questions).length){
 
   //     timer = document.getElementById("timer").innerText;
-     
+
   //     timer --;
   //     if(timer === 10){
-        
+
   //     }
   //   }
   //   //This is if you cannot think of an answer in time.
   //   else if(timer === -1){
   //     unanswered++;
-      
+
   //     clearInterval(IdTimer);
   //     resultId = setTimeout(theResults() , 1000);
   //     document.getElementById("final-results").innerHTML;
@@ -205,10 +190,9 @@ var questions =[{
   //     var element = document.getElementById("final-results");
   //     element.appendChild(finalPara);
   //     hiddenGame();
-  //     theResults();  
+  //     theResults();
 
   //   }
-    
 
   // }
   // //This Im trying to hide the game itself and keep the results.
@@ -225,7 +209,7 @@ var questions =[{
   //   IdResults = setTimeout(theResults, 1000);
   //   //This is our answers array
   //   var currentAnswer = Object.values(answers)[currentQuestion];
-    
+
   //   //This is whenever you guess correctly on the function.
   //   if(currentAnswer === correctGuess) {
   //     //this.classList.add("btn-success");
@@ -250,7 +234,7 @@ var questions =[{
   //   }
   // }
   // function theResults() {
-    
+
   //   currentQuestion++;
   //   document.getElementById("choices").style.display = "none";
   //   document.getElementById("final-results").style.display = "none";
@@ -268,18 +252,4 @@ var questions =[{
   //      document.body.appendChild(btn);
   //       }
   //   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
+})
